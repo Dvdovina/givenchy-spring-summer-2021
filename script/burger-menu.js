9c8edb2fabcf6d3a2b413da9d28d7c035e63d17f
Daria Vdovina <vdovinadn@gmail.com>
2024-08-01 07:01:45 +0300

    style: formatting

--- script/burger-menu.js
+++ script/burger-menu.js
@@ -1,27 +1,25 @@
 //Burger-Menu
 //Variables
-const popupMenu = document.querySelector('.popup-menu');
-const popupCloseBtn = popupMenu.querySelector('.popup-menu__close-btn')
-const popupOpenBtn = document.querySelector('.header__navigation-btn')
-
+const popupMenu = document.querySelector(".popup-menu");
+const popupCloseBtn = popupMenu.querySelector(".popup-menu__close-btn");
+const popupOpenBtn = document.querySelector(".header__navigation-btn");
 
 //Universal function to open popups
 function openPopup(popup) {
-  popup.classList.add('popup-menu_opened')
-};
+  popup.classList.add("popup-menu_opened");
+}
 
 //Universal function to close popups
 function closePopup(popup) {
-  popup.classList.remove('popup-menu_opened')
-};
+  popup.classList.remove("popup-menu_opened");
+}
 
 // Open Burger-Menu
-popupOpenBtn.addEventListener('click', function () {
+popupOpenBtn.addEventListener("click", function () {
   openPopup(popupMenu);
 });
 
 //Close Burger-Menu
-popupCloseBtn.addEventListener('click', function () {
+popupCloseBtn.addEventListener("click", function () {
   closePopup(popupMenu);
 });
-
--- script/random-fade-in-out.js
+++ script/random-fade-in-out.js
@@ -1,18 +1,35 @@
 const image = document.getElementById("image");
-const image2 = document.getElementById("image2")
-const image3 = document.getElementById("image3")
-const image4 = document.getElementById("image4")
-const image5 = document.getElementById("image5")
-const image6 = document.getElementById("image6")
-const image7 = document.getElementById("image7")
-const image8 = document.getElementById("image8")
-const image9 = document.getElementById("image9")
-const image10 = document.getElementById("image10")
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const image4 = document.getElementById("image4");
+const image5 = document.getElementById("image5");
+const image6 = document.getElementById("image6");
+const image7 = document.getElementById("image7");
+const image8 = document.getElementById("image8");
+const image9 = document.getElementById("image9");
+const image10 = document.getElementById("image10");
 
-let images = ["./images/looks/look_lime-bkg.png", "./images/looks/nobkg.png", "./images/looks/nobkg.png", "./images/looks/nobkg.png", "./images/looks/nobkg.png", "./images/looks/nobkg.png", "./images/looks/look_beige-bkg.png", "./images/looks/look_yellow-bkg.png", "./images/looks/look_white-bkg.png", "./images/looks/look_orange-bkg.png", "./images/looks/look_red-bkg.png", "./images/looks/look_off-white-bkg.svg", "./images/looks/look_light-blue-bkg.svg", "./images/looks/look_pink-bkg.png", "./images/looks/look_blue-bkg.png",], i = 0;
+let images = [
+    "./images/looks/look_lime-bkg.png",
+    "./images/looks/nobkg.png",
+    "./images/looks/nobkg.png",
+    "./images/looks/nobkg.png",
+    "./images/looks/nobkg.png",
+    "./images/looks/nobkg.png",
+    "./images/looks/look_beige-bkg.png",
+    "./images/looks/look_yellow-bkg.png",
+    "./images/looks/look_white-bkg.png",
+    "./images/looks/look_orange-bkg.png",
+    "./images/looks/look_red-bkg.png",
+    "./images/looks/look_off-white-bkg.svg",
+    "./images/looks/look_light-blue-bkg.svg",
+    "./images/looks/look_pink-bkg.png",
+    "./images/looks/look_blue-bkg.png",
+  ],
+  i = 0;
 
 function volgendefoto() {
-  i < images.length ? i += 1 : i = 0;
+  i < images.length ? (i += 1) : (i = 0);
   let n = Math.floor(Math.random() * 15);
   let a = Math.floor(Math.random() * 15);
   let b = Math.floor(Math.random() * 15);
